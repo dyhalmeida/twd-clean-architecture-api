1b1fc42d34f005a6f9377fd84926df5d5805ab1f
dyhalmeida <dyhalmeida@gmail.com>
2022-01-07 20:25:01 -0300

    refactor: move UserData to entities layer

--- src/tests/usescases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/tests/usescases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -1,5 +1,5 @@
 import { InMemoryUserRepository } from '../../../../usecases/register-user-on-mailing-list/repository/in-memory-user-repository'
-import { UserData } from '../../../../usecases/register-user-on-mailing-list/user-data'
+import { UserData } from '../../../../entities/user-data'
 
 describe('In memory user repository', () => {
   it('should return null if user is not found', async () => {
