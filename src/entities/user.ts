6166caaf2dbab45ea469c41cdba0b359976c56d4
dyhalmeida <dyhalmeida@gmail.com>
2022-01-07 22:53:01 -0300

    feat: add create a valid user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -6,11 +6,23 @@ import { Name } from './name'
 import { UserData } from './user-data'
 
 export class User {
+  public readonly name: Name
+  public readonly email: Email
+
+  constructor (name: Name, email: Email) {
+    this.name = name
+    this.email = email
+  }
+
   static create (user: UserData): Either<InvalidNameError | InvalidEmailError, User> {
     const emailOrError = Email.create(user.email)
     const nameOrError = Name.create(user.name)
     if (emailOrError.isLeft()) return left(new InvalidEmailError())
     if (nameOrError.isLeft()) return left(new InvalidNameError())
-    return right(user)
+
+    const name = nameOrError.value
+    const email = emailOrError.value
+
+    return right(new User(name, email))
   }
 }
